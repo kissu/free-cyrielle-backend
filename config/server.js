75110264be66ae2d2567f098dce13297a0a60a88
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-02-24 02:50:39 +0100

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '09a28bf513b92604b1c5215bd3ff0bc8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ec65789b-6004-4575-a6cc-abab42a6bc2e'
+};
